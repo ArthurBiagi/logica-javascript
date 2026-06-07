619ba9c3ef7f86e9622e3145d9f547d46c52df70
Arthur Biagi <arthurvalsezia@gmail.com>
2026-06-06 22:08:07 -0300

    feat: adicionar função para gerar número aleatorio

--- logica-js-funcoes-listas/app.js
+++ logica-js-funcoes-listas/app.js
@@ -15,3 +15,7 @@ exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
 function verificarChute() {
     console.log('O botão foi clicado!');
 }
+
+function gerarNumAleatorio(){
+    parseInt(Math.random() * 10 + 1)
+}
