f3fc6061088f30f4bd19e389702b37b9e4ec5893
Arthur Biagi <arthurvalsezia@gmail.com>
2026-06-06 22:13:22 -0300

    feat: função com retorno de numero aleatorio gerado

--- logica-js-funcoes-listas/app.js
+++ logica-js-funcoes-listas/app.js
@@ -1,21 +1,17 @@
-//let titulo = document.querySelector('h1');
-//titulo.innerHTML = 'Jogo do número Secreto';
-
-//let paragrafo = document.querySelector('p');
-//paragrafo.innerHTML = 'Escolha um número entre 1 e 10';
+let numeroSecreto = gerarNumAleatorio();
 
 function exibirTextoNaTela(tag, texto) {
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
-}
+};
 
 exibirTextoNaTela('h1', 'Jogo do Número Secreto');
 exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
 
 function verificarChute() {
     console.log('O botão foi clicado!');
-}
+};
 
 function gerarNumAleatorio(){
-    parseInt(Math.random() * 10 + 1)
-}
+    return parseInt(Math.random() * 10 + 1)
+};
