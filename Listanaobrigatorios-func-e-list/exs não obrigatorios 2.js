2ea95421f23c8104263fbc5dee718fe06261b954
Arthur Biagi <arthurvalsezia@gmail.com>
2026-06-08 20:07:58 -0300

    feat: resolução dos exs

--- Listanaobrigatorios-func-e-list/exs não obrigatorios 2.js
+++ Listanaobrigatorios-func-e-list/exs não obrigatorios 2.js
@@ -1,3 +1,4 @@
+exibirOlaMundo();
 function exibirOlaMundo(){
     alert("Olá Mundo")
 }
@@ -5,15 +6,51 @@ function exibirOlaMundo(){
 //////////////////////////////////////////////////////////////////////////////////
 
 let Nome = prompt('Digite o seu Nome:');
-let ExibeTexto = exibirOlaNome();
+exibirOlaNome(Nome);
 function exibirOlaNome(Nome){
     return alert(`Olá, ${Nome}`)
 };
 
 //////////////////////////////////////////////////////////////////////////////////
 
-let N1 = prompt('Escreva um número:');
-let dobro = dobroDoN1();
+let N1 = parseInt(prompt('Escreva um número:'));
+alert(dobrarON1(N1));
 function dobrarON1(N1){
-    return N1 * 2
+    return N1 * 2;
+}
+
+//////////////////////////////////////////////////////////////////////////////////
+
+let Pnum = parseInt(prompt('Digite o primeiro Número:'));
+let Snum = parseInt(prompt('Digite o segundo Número:'));
+let Tnum = parseInt(prompt('Digite o terceiro Número:'));
+
+alert(calcularMedia(Pnum, Snum, Tnum));
+function calcularMedia(Pnum, Snum, Tnum){
+    let Media = (Pnum + Snum + Tnum) / 3;
+    return Media;
+}
+
+//////////////////////////////////////////////////////////////////////////////////
+
+let fnum = parseInt(prompt('Digite o primeiro Número:'));
+let lnum = parseInt(prompt('Digite o segundo Número:'));
+
+alert(exibirMaiorNum(fnum, lnum));
+function exibirMaiorNum(fnum, lnum){
+    if (fnum > lnum){
+        return `O maior número é o ${fnum}`
+    } else if(lnum > fnum){
+        return `O maior número é o ${lnum}`
+    } else{
+        return 'Os dois números são iguais'
+    }
+}
+
+//////////////////////////////////////////////////////////////////////////////////
+
+let Anum = parseInt(prompt('Escreva um número:'));
+alert(multiplicaPorEleMsm(Anum));
+function multiplicaPorEleMsm(Anum){
+    return Anum * Anum;
 }
