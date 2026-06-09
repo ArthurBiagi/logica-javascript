1e0da680083f144b5a77ada90809796578b8d1ac
Arthur Biagi <arthurvalsezia@gmail.com>
2026-06-09 19:27:44 -0300

    feat: verificação de chute

--- logica-js-funcoes-listas/app.js
+++ logica-js-funcoes-listas/app.js
@@ -10,7 +10,15 @@ exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
 
 function verificarChute() {
     let chute = document.querySelector('input').value;
-    console.log(chute == numeroSecreto);
+
+    if (chute == numeroSecreto) {
+        exibirTextoNaTela('h1', 'Parabéns! Você acertou o número secreto!');
+        exibirTextoNaTela('p', '');
+    } else if (chute < numeroSecreto) {
+        exibirTextoNaTela('p', 'O número secreto é maior')
+    } else {
+        exibirTextoNaTela('p', 'O número secreto é menor')
+    }
 };
 
 function gerarNumAleatorio() {
