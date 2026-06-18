87c4346a364a4f3347f70adf4f3829890c442bb5
Arthur Biagi <arthurvalsezia@gmail.com>
2026-06-17 21:14:47 -0300

    feat: criação da função exibirtexto

--- Listanaobrigatorios-func-e-list/exs não obrigatorios 3.js
+++ Listanaobrigatorios-func-e-list/exs não obrigatorios 3.js
@@ -1,8 +1,12 @@
 let Altura = prompt('Indique a altura da pessoa:');
 let Peso = prompt('Indique o peso da pessoa:');
 
+function exibirTexto(tag, texto) {
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+}
 
 function calculoIMC(Altura, Peso) {
     let IMC = Peso / (Altura * Altura);
-    return IMC;
+    exibirTexto('H1', `O IMC é ${IMC}`);
 }
