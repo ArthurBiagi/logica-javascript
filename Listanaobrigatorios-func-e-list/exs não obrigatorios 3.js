455cd440a62b73b4472facce020cb968254b0a64
Arthur Biagi <arthurvalsezia@gmail.com>
2026-06-29 21:47:03 -0300

    feat: getelementbyid do input

--- Listanaobrigatorios-func-e-list/exs não obrigatorios 3.js
+++ Listanaobrigatorios-func-e-list/exs não obrigatorios 3.js
@@ -1,11 +1,10 @@
-
-
 function exibirTexto(tag, texto) {
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
 }
 
-function calculoIMC(Altura, Peso) {
+function calculoIMC() {
+    let Altura = parseFloat(document.getElementById('alturaInput'))
     let IMC = Peso / (Altura * Altura);
     exibirTexto('H1', `O IMC é ${IMC}`);
 }
