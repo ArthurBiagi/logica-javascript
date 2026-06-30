678f2b426fe9574c46b27dd451ddbaa66e6b4c57
Arthur Biagi <arthurvalsezia@gmail.com>
2026-06-29 21:41:16 -0300

    feat: altura em metros

--- Listanaobrigatorios-func-e-list/exs não obrigatorios 3.js
+++ Listanaobrigatorios-func-e-list/exs não obrigatorios 3.js
@@ -1,4 +1,4 @@
-let Altura = prompt('Indique a altura da pessoa (em cm):');
+let Altura = prompt('Indique a altura da pessoa:');
 let Peso = prompt('Indique o peso da pessoa (em kg):');
 
 function exibirTexto(tag, texto) {
