58e5c32158c15abea4041829eab52fcb00f1e793
Arthur Biagi <arthurvalsezia@gmail.com>
2026-06-29 21:57:46 -0300

    feat: função de cálculo de fatorial

--- Listanaobrigatorios-func-e-list/exs não obrigatorios 3.js
+++ Listanaobrigatorios-func-e-list/exs não obrigatorios 3.js
@@ -7,10 +7,14 @@ function calculoIMC() {
     let Altura = parseFloat(document.getElementById('alturaInput').value);
     let Peso = parseFloat(document.getElementById('pesoInput').value);
 
-    if (Altura > 0 && peso > 0){
+    if (Altura > 0 && Peso > 0){
     let IMC = Peso / (Altura * Altura);
     exibirTexto('p', `O IMC é ${IMC.toFixed(2)}`);
     } else{
     exibirTexto('p', `Valores inválidos`);
     }
+}
+
+function calculoFatorial(N1){
+    
 }
